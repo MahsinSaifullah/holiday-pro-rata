9c78b5dd5500150896cc32c3960a5b8f6ec01f94
macbookpro <mahsinsaifullah@gmail.com>
2024-01-13 15:29:30 +0000

    feat: convert input field to controlled input with states

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,76 @@
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
+import { Dayjs } from 'dayjs';
 import './form.scss';
 
+interface FormData {
+  leavePerAnnum?: number;
+  dateFrom?: Dayjs | null;
+  dataTo?: Dayjs | null;
+  workCapacity?: number;
+}
+
 export const Form = () => {
+  const [formData, setFormData] = useState<FormData | null>(null);
+
+  const handleOnChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: +value,
+    });
+  };
+
   return (
     <Card>
       <CardContent>
         <div className="input-container">
           <h3>Leave entitlement per annum:</h3>
-          <TextField />
+          <TextField
+            name="leavePerAnnum"
+            type="number"
+            value={formData?.leavePerAnnum}
+            onChange={handleOnChange}
+          />
           <h3>days</h3>
         </div>
         <div className="input-container">
           <h3>Dates to calculate:</h3>
-          <DatePicker label="From" />
-          <DatePicker label="To" />
+          <DatePicker
+            label="From"
+            onChange={(value: Dayjs | null) => {
+              setFormData({
+                ...formData,
+                dateFrom: value,
+              });
+            }}
+          />
+          <DatePicker
+            label="To"
+            onChange={(value: Dayjs | null) => {
+              setFormData({
+                ...formData,
+                dataTo: value,
+              });
+            }}
+          />
         </div>
         <div className="input-container">
           <h3>Work capacity:</h3>
-          <TextField />
+          <TextField
+            name="workCapacity"
+            type="number"
+            error={!!formData?.workCapacity && formData.workCapacity > 100}
+            onChange={handleOnChange}
+          />
           <h3>%</h3>
         </div>
       </CardContent>
